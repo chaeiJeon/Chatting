004455d5174bac5d63ff32113f28102cc833a1b5
chaeiJeon <ahswn1031@gmail.com>
2023-05-29 21:56:34 +0900

    feat: addUser action 추가

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const clients = require("./clients");
+const clients_websocket = require("./clients_websocket");
+let clients = [];
 
 const app = express();
 const port_server = 8080;
@@ -13,18 +14,26 @@ app.use(cors(corsOptions));
 app.listen(port_server);
 
 //redis
-const { _connect, _subscribe, setSender } = require("./redis");
+const { _connect, _publish, _subscribe, setSender } = require("./redis");
 let channelNum = 0;
 let channelName = `channel:${channelNum}`;
 
 // api
 const WebSocketServer = require("./WebSocketServer.js");
+let webSocketServer = new WebSocketServer(1234, channelName);
+_connect();
+_subscribe(channelName);
 app.post("/open", (req, res) => {
   let { user } = req.body;
+  console.log(clients);
   setSender(user);
-  _subscribe(channelName);
-  let webSocketServer = new WebSocketServer(1234, channelName);
-  _connect();
-
-  res.send({ data: clients.length });
+  clients.push(user);
+  _publish(
+    channelName,
+    JSON.stringify({
+      action: "addUser",
+      name: user,
+      clients: clients,
+    })
+  );
 });
